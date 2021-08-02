e78dd2dabb4bf0cb49e389b9a85a486d454854a2
Babybroker <kevin.d@live.nl>
2021-08-02 16:45:00 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ml_aid',
+    version='0.1',
+    packages=['pandas', 'numpy'],
+    url='',
+    license='CC BY-SA',
+    author='K. Dankers',
+    author_email='kevin.d@live.nl',
+    description='Functions to aid ML processes'
+)
