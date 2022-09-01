2b4ddc28995f6a2d844bba6ca4c97b3467bb1c5a
kevin <kevin.d@live.nl>
2022-09-01 18:04:01 +0200

    Small fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mlaid',
-    version='0.1',
+    version='0.1.1',
     description='A python package providing forecasting functions for solar production',
 
     # url='https://github.com/EnergieID/entsoe-py',
