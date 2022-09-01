033fff29b15f19919220acfb96a22f64cdb3621d
kevin <kevin.d@live.nl>
2022-09-01 17:35:59 +0200

    Add gitignore

--- setup.py
+++ setup.py
@@ -1,13 +1,43 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='mlaid',
     version='0.1',
-    packages=[],
-    url='',
-    license='CC BY-SA',
+    description='A python package providing forecasting functions for solar production',
+
+    # url='https://github.com/EnergieID/entsoe-py',
+    license='MIT',
     author='K. Dankers',
-    author_email='kevin.d@live.nl',
-    description='Functions to aid ML processes',
-    install_requires=['xgboost', 'pandas>=1.4.0'],
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    # classifiers=[
+    #     'Development Status :: 5 - Production/Stable',
+    #
+    #     # Indicate who your project is intended for
+    #     'Intended Audience :: Developers',
+    #     'Topic :: Scientific/Engineering',
+    #
+    #     # Pick your license as you wish (should match "license" above)
+    #     'License :: OSI Approved :: MIT License',
+    #
+    #     # Specify the Python versions you support here. In particular, ensure
+    #     # that you indicate whether you support Python 2, Python 3 or both.
+    #     'Programming Language :: Python :: 3.9',
+    #     'Programming Language :: Python :: 3.10',
+    # ],
+
+    keywords='Solar Forecast',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=find_packages(),
+
+    # Alternatively, if you want to distribute just a my_module.py, uncomment
+    # this:
+    #py_modules=[""],
+
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed.
+    install_requires=['fsspec', 'pandas>=1.4.0'],
+
+    include_package_data=True,
 )
